1dd89c6254f8405cb4c861e4d36e5d9ab03ec79d
btd <cdrom205@gmail.com>
2026-05-10 20:29:29 +0100

    Fix a bug when rabbitMQ down can cause the app to crash due to producer not handling exception

--- tenvia-app/src/main/java/com/tenvia/services/ScoreProducer.java
+++ tenvia-app/src/main/java/com/tenvia/services/ScoreProducer.java
@@ -1,10 +1,13 @@
 package com.tenvia.services;
 
 import com.tenvia.common.event.ScoreSubmittedEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class ScoreProducer {
 
@@ -12,6 +15,10 @@ public class ScoreProducer {
     private RabbitTemplate rabbitTemplate;
 
     public void sendUpdate(ScoreSubmittedEvent event) {
-        rabbitTemplate.convertAndSend("game.exchange", "score.submitted", event);
+        try {
+            rabbitTemplate.convertAndSend("game.exchange", "score.submitted", event);
+        } catch(AmqpException ex) {
+            log.error("Fail to send score update");
+        }
     }
 }
