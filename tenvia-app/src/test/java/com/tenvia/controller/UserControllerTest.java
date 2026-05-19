b2ff07d9d0fa587df616f417a8807a0b55592338
btd <cdrom205@gmail.com>
2026-05-19 19:39:58 +0100

    Test coverage

---
+++ tenvia-app/src/test/java/com/tenvia/controller/UserControllerTest.java
@@ -0,0 +1,61 @@
+package com.tenvia.controller;
+
+
+import com.tenvia.entities.UserEntity;
+import com.tenvia.services.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private MockMvc mockMvc;
+    @MockitoBean
+    private UserService userService;
+
+    @Test
+    void canLogin() throws Exception {
+        String username = "alice";
+        UserEntity userEntity = new UserEntity(username);
+        when(userService.login(username)).thenReturn(userEntity);
+
+        String responseData = mockMvc.perform(post("/users/login")
+                        .param("username", username))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        UserEntity userResponse = objectMapper.readValue(responseData, UserEntity.class);
+        assertThat(userResponse.getUsername()).isEqualTo("alice");
+        assertThat(userResponse.getBalance()).isEqualTo(0);
+    }
+
+    @Test
+    void expect400_whenLogin_userNameIsNull() throws Exception {
+        mockMvc.perform(post("/users/login"))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    void expect400_whenLogin_usernameIsBlank() throws Exception {
+        String blankUsername = " ";
+        mockMvc.perform(post("/users/login/")
+                        .param("username", blankUsername))
+                .andExpect(status().is4xxClientError());
+    }
+}
