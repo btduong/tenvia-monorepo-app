d757d6db32edf2bcbaaa234339ba0f411976acac
btd <cdrom205@gmail.com>
2026-05-19 23:31:15 +0100

    Improve test

--- leaderboard-service/src/test/java/com/tenvia/leaderboard/controller/LeaderboardControllerTest.java
+++ leaderboard-service/src/test/java/com/tenvia/leaderboard/controller/LeaderboardControllerTest.java
@@ -1,6 +1,5 @@
 package com.tenvia.leaderboard.controller;
 
-import com.tenvia.leaderboard.controller.LeaderboardController;
 import com.tenvia.leaderboard.dto.LeaderboardDTO;
 import com.tenvia.leaderboard.service.LeaderboardService;
 import org.assertj.core.util.Lists;
@@ -9,9 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -21,8 +23,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class LeaderboardControllerTest {
 
     @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
     private MockMvc mockMvc;
-
     @MockitoBean
     private LeaderboardService leaderboardService;
 
@@ -34,13 +37,20 @@ class LeaderboardControllerTest {
 
         when(leaderboardService.getTopScores()).thenReturn(mockScores);
 
-        mockMvc.perform(get("/leaderboard"))
+        String responseData = mockMvc.perform(get("/leaderboard"))
                 .andExpect(status().is2xxSuccessful())
-                .andExpect(jsonPath("$.length()").value(2))
-                .andExpect(jsonPath("$[0].userName").value("Alice"))
-                .andExpect(jsonPath("$[0].score").value(15))
-                .andExpect(jsonPath("$[1].userName").value("Bob"))
-                .andExpect(jsonPath("$[1].score").value(10));;
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        List<LeaderboardDTO> topScores = objectMapper.readValue(responseData, new TypeReference<>() {
+        });
+        assertThat(topScores.size()).isEqualTo(2);
+        assertThat(topScores.get(0).userName()).isEqualTo("Alice");
+        assertThat(topScores.get(0).score()).isEqualTo(15);
+        assertThat(topScores.get(1).userName()).isEqualTo("Bob");
+        assertThat(topScores.get(1).score()).isEqualTo(10);
+
     }
 
     @Test
